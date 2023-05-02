826dbee9c918dd8814cc5064c2c86b5eca44339a
Bhaskar <sgorle@ctepl.com>
2023-05-02 14:32:19 +0530

    Update PointTrackerResourceUTest.java

--- src/test/java/com/hillspet/wearables/jaxrs/resource/PointTrackerResourceUTest.java
+++ src/test/java/com/hillspet/wearables/jaxrs/resource/PointTrackerResourceUTest.java
@@ -1,6 +1,6 @@
 package com.hillspet.wearables.jaxrs.resource;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
